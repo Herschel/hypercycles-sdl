f12728ac1f8c1269541eebd06353b9be95e77560
Mike Welsh <mwelsh@gmail.com>
2018-10-03 12:27:19 -0700

    Add GPL3 header to source files

--- src/main.c
+++ src/main.c
@@ -1,5 +1,22 @@
+/*
+* hypercycles-sdl (https://github.com/Herschel/hypercycles-sdl)
+* Copyright (c) 2015 The hypercycles-sdl authors
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, version 3.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdio.h>
 #include "SDL.h"
+#include "SDL_mixer.h"
 
 extern void main_hyper6(void);
 
--- src/timer.h
+++ src/timer.h
@@ -1,3 +1,19 @@
+/*
+* hypercycles-sdl (https://github.com/Herschel/hypercycles-sdl)
+* Copyright (c) 2015 The hypercycles-sdl authors
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, version 3.
+*
+* This program is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 /* TIMER.H - Header file for TIMER.C                               */
 /* By Jim Dos‚.  This program is released to the public domain and */
 /* may be used in your own programs.  Leave me a note if you find  */
