01490ae69ca941193077c9cb5404cc235c5dd2f3
Mike Welsh <mwelsh@gmail.com>
2018-10-07 12:16:08 -0700

    Switch language to C++ in premake configuration
    In premake5.lua, changed language to C++ in premake5.lua, cleaned
    up the indentation, and added GPL3 header.

--- premake5.lua
+++ premake5.lua
@@ -1,49 +1,63 @@
--- premake5.lua
+-- hypercycles-sdl (https://github.com/Herschel/hypercycles-sdl)
+-- Copyright (c) 2015 The hypercycles-sdl authors
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, version 3.
+--
+-- This program is distributed in the hope that it will be useful, but
+-- WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+-- General Public License for more details.
+-- 
+-- You should have received a copy of the GNU General Public License
+-- along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 workspace "hypercycles"
-   configurations { "debug", "release" }
-   if os.target() == "windows" then
-      platforms { "win64" }
-   else
-      platforms { "osx" }
-   end
+  language "C++"
+  configurations { "debug", "release" }
+  if os.target() == "windows" then
+    platforms { "win64" }
+  else
+    platforms { "osx" }
+  end
 
-   -- Windows SDK version.
-   -- Must be set outside filter.
-   systemversion "10.0.15063.0"
+  -- Windows SDK version.
+  -- Must be set outside filter.
+  systemversion "10.0.15063.0"
 
-   filter "configurations:debug"
-      defines { "DEBUG" }
-      symbols "On"
+  filter "configurations:debug"
+    defines { "DEBUG" }
+    symbols "On"
 
-    filter "configurations:release"
-      defines { "NDEBUG" }
-      optimize "On"
+  filter "configurations:release"
+    defines { "NDEBUG" }
+    optimize "On"
 
-    filter "platforms:win64"
-      system "Windows"
-      architecture "x64"
+  filter "platforms:win64"
+    system "Windows"
+    architecture "x64"
 
-    filter "platforms:osx"
-      system "macosx"
-      libdirs { "/usr/local/lib" }
-      includedirs { "/usr/local/include/SDL2" }
-      links { "SDL2", "SDL2_mixer" }
+  filter "platforms:osx"
+    system "macosx"
+    libdirs { "/usr/local/lib" }
+    includedirs { "/usr/local/include/SDL2" }
+    links { "SDL2", "SDL2_mixer" }
 
 
-project "hypercycles"
-  kind "ConsoleApp"
-  language "C"
+  project "hypercycles"
+    kind "ConsoleApp"
 
-  targetdir "bin/%{cfg.buildcfg}"
-  debugdir "assets"
+    targetdir "bin/%{cfg.buildcfg}"
+    debugdir "assets"
 
-  nuget {
-    "sdl2.nuget:2.0.8.2",
-    "sdl2.nuget.redist:2.0.8.2",
-    "sdl2_mixer.nuget:2.0.2.2",
-    "sdl2_mixer.nuget.redist:2.0.2.2"
-  }
+    -- Windows only, but must be set outside filter.
+    nuget {
+      "sdl2.nuget:2.0.8.2",
+      "sdl2.nuget.redist:2.0.8.2",
+      "sdl2_mixer.nuget:2.0.2.2",
+      "sdl2_mixer.nuget.redist:2.0.2.2"
+    }
 
-  files { "src/*.c", "src/*.cpp", "src/*.h" }
-  excludes { "src/build1.cpp" }
+    files { "src/*.c", "src/*.cpp", "src/*.h" }
+    excludes { "src/build1.cpp" }
